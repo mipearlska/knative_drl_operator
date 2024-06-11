ddab65edde17ea310f136085f7a6743253453e2b
Trần Minh Ngọc <mipearlska1307@dcn.ssu.ac.kr>
2024-06-11 19:25:31 +0900

    Add files via upload

---
+++ prequisites_traffic_generator_files/2min_number_service.py
@@ -0,0 +1,75 @@
+from locust import HttpUser, TaskSet, task, between, constant
+from locust import LoadTestShape
+
+class QuickstartUser(HttpUser):
+    wait_time = constant(0)
+
+    @task(1)
+    def index_page(self):
+        response = self.client.post("/recognize_number")
+
+class StagesShape(LoadTestShape):
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+    Keyword arguments:
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages = [
+#        {"duration": 120, "users": 19, "spawn_rate": 19},
+#        {"duration": 240, "users": 28, "spawn_rate": 28},
+#        {"duration": 360, "users": 70, "spawn_rate": 70},
+#        {"duration": 480, "users": 30, "spawn_rate": 30},
+#        {"duration": 600, "users": 27, "spawn_rate": 27},
+#        {"duration": 720, "users": 78, "spawn_rate": 78},
+#        {"duration": 840, "users": 55, "spawn_rate": 55},
+#        {"duration": 960, "users": 47, "spawn_rate": 47},
+#        {"duration": 1080, "users": 44, "spawn_rate": 44},
+#        {"duration": 1200, "users": 33, "spawn_rate": 33},
+#        {"duration": 1320, "users": 34, "spawn_rate": 34},
+#        {"duration": 1440, "users": 50, "spawn_rate": 50},
+#        {"duration": 1560, "users": 66, "spawn_rate": 66},
+#        {"duration": 1680, "users": 66, "spawn_rate": 66},
+#        {"duration": 1800, "users": 69, "spawn_rate": 69},
+#        {"duration": 1920, "users": 94, "spawn_rate": 94},
+#        {"duration": 2040, "users": 53, "spawn_rate": 53},
+#        {"duration": 2160, "users": 56, "spawn_rate": 56},
+#        {"duration": 2280, "users": 86, "spawn_rate": 86},
+#        {"duration": 2400, "users": 100, "spawn_rate": 100},
+        {"duration": 120, "users": 19, "spawn_rate": 19},
+        {"duration": 240, "users": 16, "spawn_rate": 16},
+        {"duration": 360, "users": 25, "spawn_rate": 25},
+        {"duration": 480, "users": 47, "spawn_rate": 47},
+        {"duration": 600, "users": 33, "spawn_rate": 33},
+        {"duration": 720, "users": 15, "spawn_rate": 15},
+        {"duration": 840, "users": 19, "spawn_rate": 19},
+        {"duration": 960, "users": 13, "spawn_rate": 13},
+        {"duration": 1080, "users": 25, "spawn_rate": 25},
+        {"duration": 1200, "users": 50, "spawn_rate": 50},
+        {"duration": 1320, "users": 40, "spawn_rate": 40},
+        {"duration": 1440, "users": 66, "spawn_rate": 66},
+        {"duration": 1560, "users": 35, "spawn_rate": 35},
+        {"duration": 1680, "users": 55, "spawn_rate": 55},
+        {"duration": 1800, "users": 80, "spawn_rate": 80},
+        {"duration": 1920, "users": 33, "spawn_rate": 33},
+        {"duration": 2040, "users": 41, "spawn_rate": 41},
+        {"duration": 2160, "users": 22, "spawn_rate": 22},
+        {"duration": 2280, "users": 16, "spawn_rate": 16},
+        {"duration": 2400, "users": 18, "spawn_rate": 18},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
+
+        return None
